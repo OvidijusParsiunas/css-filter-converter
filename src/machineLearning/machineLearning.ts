215d7c629350002ae4132aef1b2276ca9c70632c
OvidijusParsiunas <oparsiunas@googlemail.com>
2022-01-14 17:17:43 +0000

    generate more data for ml

--- src/machineLearning/machineLearning.ts
+++ src/machineLearning/machineLearning.ts
@@ -3,8 +3,8 @@ import { GenerateCsv } from './generateCsv';
 
 export class MachineLearning {
   public static generateData(): void {
-    const trainingData = GenerateData.generate(20);
-    const testData = GenerateData.generate(5);
+    const trainingData = GenerateData.generate(10000);
+    const testData = GenerateData.generate(2000);
     const dataHeaders = ['invert', 'sepia', 'saturate', 'hue-rotate', 'brightness', 'contrast'];
     const labelHeaders = ['result'];
     GenerateCsv.generate(trainingData.data, dataHeaders, 'training-data');
