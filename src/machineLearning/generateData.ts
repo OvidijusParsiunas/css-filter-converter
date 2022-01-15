60514cfd55ee365d154da78635f8c59b10f87c62
OvidijusParsiunas <oparsiunas@googlemail.com>
2022-01-15 23:46:35 +0000

    generating the green value in rgbfor machine learning data

--- src/machineLearning/generateData.ts
+++ src/machineLearning/generateData.ts
@@ -1,19 +1,24 @@
 import { FilterCssConverter } from '../filterCssConverter/filterCssConverter';
 
+interface Data {
+  data: number[][];
+  labels: number[][];
+}
+
 export class GenerateData {
   private static NUMBER_PATTERN = /\d+/g;
 
-  public static generate(numberOfItems: number): { data: number[][]; labels: number[][] } {
+  public static generate(numberOfItems: number): Data {
     const dataArray: number[][] = [];
     const labelsArray: number[][] = [];
     for (let i = 0; i < numberOfItems; i += 1) {
-      const randomNumber = Math.floor(Math.random() * 255);
-      const rgb = `rgb(${randomNumber},255,0)`;
+      const number = i % 255;
+      const rgb = `rgb(0,${number},0)`;
       const result = FilterCssConverter.rgbToFilter(rgb);
       const resultNumbers =
         result.filter?.match(GenerateData.NUMBER_PATTERN)?.map((stringNumber) => Number.parseInt(stringNumber)) || [];
       dataArray.push(resultNumbers?.slice(2));
-      labelsArray.push([randomNumber]);
+      labelsArray.push([number]);
     }
     return { data: dataArray, labels: labelsArray };
   }
