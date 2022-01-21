f6c4d63926646830dfb992d9c2901e0d36f7cc95
OvidijusParsiunas <oparsiunas@googlemail.com>
2022-01-21 21:10:06 +0000

    update machine learning code

--- src/machineLearning/generateData.ts
+++ src/machineLearning/generateData.ts
@@ -22,4 +22,37 @@ export class GenerateData {
     }
     return { data: dataArray, labels: labelsArray };
   }
+
+  public static generateProcedurallyToMaxIndex(maxIndex: number): { training: Data; test: Data } {
+    const trainingData: number[][] = [];
+    const trainingLabels: number[][] = [];
+    const testData: number[][] = [];
+    const testLabels: number[][] = [];
+    let testDataIndex = 0;
+    let currentNumber = 1;
+    for (let i = 1; i < maxIndex; i += 1) {
+      for (let y = 1; y < maxIndex; y += 1) {
+        for (let z = 1; z < maxIndex; z += 1) {
+          const rgb = `rgb(${i},${y},${z})`;
+          const result = FilterCssConverter.rgbToFilter(rgb);
+          const resultNumbers =
+            result.filter?.match(GenerateData.NUMBER_PATTERN)?.map((stringNumber) => Number.parseInt(stringNumber)) ||
+            [];
+          if (testDataIndex === 5) {
+            testData.push(resultNumbers?.slice(2));
+            testLabels.push([currentNumber]);
+            testDataIndex = 0;
+          } else {
+            trainingData.push(resultNumbers?.slice(2));
+            trainingLabels.push([currentNumber]);
+          }
+          testDataIndex += 1;
+          currentNumber += 1;
+        }
+      }
+    }
+    const training = { data: trainingData, labels: trainingLabels };
+    const test = { data: testData, labels: testLabels };
+    return { training, test };
+  }
 }
