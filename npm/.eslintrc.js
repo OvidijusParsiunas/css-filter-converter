5b37938e8ed4d9c117c22c27fc96210cbe1ad824
OvidijusParsiunas <oparsiunas@googlemail.com>
2022-01-22 22:16:11 +0000

    moved files from parent directory to npm

--- npm/.eslintrc.js
+++ npm/.eslintrc.js
@@ -3,29 +3,26 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'airbnb-typescript/base',
-    'plugin:import/recommended',
-  ],
+  extends: ['airbnb-typescript/base', 'plugin:import/recommended'],
   parser: '@typescript-eslint/parser',
   overrides: [
     {
       files: ['*.ts'],
       parserOptions: {
-        project: './tsconfig.json',
+        project: 'tsconfig.json',
       },
     },
   ],
   rules: {
     '@typescript-eslint/no-explicit-any': ['error', { ignoreRestArgs: true }],
-    'max-len': ['error', { 'code': 120 }],
+    'max-len': ['error', { code: 120 }],
     'prefer-template': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-var': 'error',
-    'prefer-const': 'error'
+    'prefer-const': 'error',
   },
-  ignorePatterns: ['lib/*', 'website/'],
+  ignorePatterns: ['lib/*'],
 };
 
 // prettier configuration has been added in the following commit - 8fa774c675edb0a44671027866aebd422bbb816f
