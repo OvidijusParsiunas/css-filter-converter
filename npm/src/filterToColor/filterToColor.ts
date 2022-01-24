7726dc3c27d3b241399a620febbd74647a601325
OvidijusParsiunas <oparsiunas@googlemail.com>
2022-01-24 01:11:45 +0000

    initial code structure refactoring work

--- npm/src/filterToColor/filterToColor.ts
+++ npm/src/filterToColor/filterToColor.ts
@@ -1,69 +1,11 @@
 export class FilterToColor {
-  private static TARGET_URL = 'https://www.google.com';
-
-  private static async evaluate(): Promise<string> {
-    const canvasSideLength = 20;
-    const fillStyle = 'blue';
-
-    function createCanvas(): HTMLCanvasElement {
-      const canvas = document.createElement('canvas');
-      canvas.width = canvasSideLength;
-      canvas.height = canvasSideLength;
-      document.body.appendChild(canvas);
-      return canvas;
-    }
-
-    function fillCanvas(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D): void {
-      context.fillStyle = fillStyle;
-      context.fillRect(0, 0, canvas.width, canvas.height);
-    }
-
-    function getCanvasImageData(context: CanvasRenderingContext2D): Uint8ClampedArray {
-      const canvasMiddleCoordinate = canvasSideLength / 2;
-      const canvasSecondaryCoordinate = canvasMiddleCoordinate + 2;
-      return context.getImageData(
-        canvasMiddleCoordinate,
-        canvasMiddleCoordinate,
-        canvasSecondaryCoordinate,
-        canvasSecondaryCoordinate,
-      ).data;
-    }
-
-    function rgbToHex(r: number, g: number, b: number): string {
-      // Error handling
-      if (r > 255 || g > 255 || b > 255) throw new Error('Invalid color component');
-      return ((r << 16) | (g << 8) | b).toString(16);
-    }
-
-    const canvas = createCanvas();
-    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-    fillCanvas(canvas, context);
-    const data = getCanvasImageData(context);
-    const hex = rgbToHex(data[0], data[1], data[2]);
-    return `#${`000000${hex}`.slice(-6)}`;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private static async getPuppeteerDependency(): Promise<any> {
-    try {
-      const anotherVariable = '';
-      // eslint-disable-next-line prefer-template
-      return require('puppeteer' + anotherVariable);
-    } catch (e: unknown) {
-      throw new Error(
-        "ERROR: To generate color values from filter in Node - you will first need to install 'puppeteer' " +
-          'by running: \n npm install puppeteer',
-      );
-    }
-  }
-
   public static async generate(): Promise<string | null> {
-    const puppeteer = await FilterToColor.getPuppeteerDependency();
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(FilterToColor.TARGET_URL);
-    const result = await page.evaluate(FilterToColor.evaluate);
-    await browser.close();
-    return result;
+    if (typeof window === 'undefined') {
+      return import('./node').then((module) => {
+        const { FilterToColorNode } = module;
+        return FilterToColorNode.generate();
+      });
+    }
+    return new Promise((resolve) => resolve('mock'));
   }
 }
--- npm/src/index.ts
+++ npm/src/index.ts
@@ -1,16 +1,33 @@
-import { FilterCssConverter } from './filterCssConverter/filterCssConverter';
+import { FilterCssGenerator } from './filterCssConverter/filterCssGenerator';
+import { ErrorHandler } from './filterCssConverter/errorHandler';
+import { FilterToColor } from './filterToColor/filterToColor';
+import { ColorStringTypes } from './types/colorStringTypes';
+import { Color } from './filterCssConverter/color';
+import { Result } from './types/result';
 
 export default class CssFilterConverter {
-  public static async convert(): Promise<string | null> {
-    if (typeof window === 'undefined') {
-      return import('./filterToColor/filterToColor').then((module) => {
-        const { FilterToColor } = module;
-        return FilterToColor.generate();
-      });
+  private static convertToFilter(colorString: string, type: ColorStringTypes): Result {
+    try {
+      const color = new Color(colorString, type);
+      const cssGenerator = new FilterCssGenerator(color);
+      return cssGenerator.generate();
+    } catch (error: unknown) {
+      return ErrorHandler.returnErrorResult(error);
     }
-    return new Promise((resolve) => {
-      const result = FilterCssConverter.hexToFilter('#ff0000').filter;
-      resolve(result);
-    });
+  }
+
+  // WORK - regex validator
+  public static hexToFilter(hex: string): Result {
+    return CssFilterConverter.convertToFilter(hex, 'hex');
+  }
+
+  // WORK - regex validator
+  public static rgbToFilter(rgb: string): Result {
+    return CssFilterConverter.convertToFilter(rgb, 'rgb');
+  }
+
+  // WORK - regex validator
+  public static async filterToRgb(): Promise<string | null> {
+    return FilterToColor.generate();
   }
 }
