2b4b4ae4683f99e5952d9a7f64f67091cf0e1dae
OvidijusParsiunas <oparsiunas@googlemail.com>
2022-02-08 19:32:50 +0000

    when hovering any of the history items, the full item row is going to have its text color changed

--- website/src/components/columns/resultColumn/history/history.tsx
+++ website/src/components/columns/resultColumn/history/history.tsx
@@ -7,28 +7,49 @@ import { useSelector } from 'react-redux';
 import CSS from 'csstype';
 import './history.css';
 
+interface HistoryItem {
+  element: HTMLElement | null;
+}
+
 // the reason why input and result history are in the same component and are placed in the same rows alongside
 // each other is because input row height needs to be exactly the same as the result height which can vary
 // with filter.
 export default function History() {
   const historyState = useSelector<RootReducer, RootReducer['history']['history']>((state) => state.history.history);
 
-  function getHistoryColumn(text: string, float: CSS.Property.Float, isResult: boolean): JSX.Element {
+  function changeTextColor(color: CSS.Property.Color, hItem: HistoryItem): void {
+    if (hItem.element) hItem.element.style.color = color;
+  }
+
+  function getHistoryColumn(text: string, float: CSS.Property.Float, isResult: boolean, hItem: HistoryItem): JSX.Element {
     return (
       <OutputText float={float} width={SIDE_COLUMN_WIDTH_PX}>
         {isResult ? <ResultHeaderText classes={['history-padding-text']} /> : null}
-        <div className="history-text">{text}</div>
+        <div
+          className="history-text"
+          onMouseEnter={() => changeTextColor('black', hItem)}
+          onMouseLeave={() => changeTextColor('grey', hItem)}
+        >
+          {text}
+        </div>
       </OutputText>
     );
   }
 
+  const setHistoryItemRef = (element: HistoryItem['element'], historyItem: HistoryItem) => {
+    historyItem.element = element;
+  };
+
   function getHistoryItems(history: HistoryElement[]): JSX.Element[] {
-    return history.map((historyElement) => (
-      <div key={historyElement.id} className="history-item">
-        {getHistoryColumn(historyElement.input, 'left', false)}
-        {getHistoryColumn(historyElement.result, 'right', true)}
-      </div>
-    ));
+    return history.map((historyElement) => {
+      const historyItem: HistoryItem = { element: null };
+      return (
+        <div key={historyElement.id} ref={(element) => setHistoryItemRef(element, historyItem)} className="history-item">
+          {getHistoryColumn(historyElement.input, 'left', false, historyItem)}
+          {getHistoryColumn(historyElement.result, 'right', true, historyItem)}
+        </div>
+      );
+    });
   }
 
   return (
