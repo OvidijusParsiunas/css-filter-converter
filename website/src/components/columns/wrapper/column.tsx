95b18ef8305098d0f0d4beb5fa0f048dd2ea722c
OvidijusParsiunas <oparsiunas@googlemail.com>
2022-02-21 17:10:29 +0000

    github workflow production fix

--- website/src/components/columns/wrapper/column.tsx
+++ website/src/components/columns/wrapper/column.tsx
@@ -5,20 +5,28 @@ import './column.css';
 type Props = {
   children: ComponentsAsProp;
   zIndex?: number;
+  columnRef?: React.RefObject<HTMLDivElement> | null;
+};
+
+const defaultProps = {
+  zIndex: 0,
+  columnRef: null,
 };
 
 // Current purpose for zIndex is to allow the color picker and switch button to be above the history items,
 // yet allow them to still be hovered. This is why it is applied to column-contents and not column div.
-const Column = React.forwardRef<HTMLDivElement, Props>(({ children, zIndex }, ref) => (
-  <div ref={ref} className="column">
-    <div style={{ zIndex }} className="column-contents">
-      {children}
+export default function Column(props: Props) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const ColumnComponent = React.forwardRef<HTMLDivElement, Props>(({ children, zIndex, columnRef }, ref) => (
+    <div ref={columnRef} className="column">
+      <div style={{ zIndex }} className="column-contents">
+        {children}
+      </div>
     </div>
-  </div>
-));
+  ));
+  ColumnComponent.defaultProps = defaultProps;
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <ColumnComponent {...props} />;
+}
 
-Column.defaultProps = {
-  zIndex: 0,
-};
-
-export default Column;
+Column.defaultProps = defaultProps;
