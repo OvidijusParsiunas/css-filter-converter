279885fbe81d6040156c61f8420f33287ff352e0
OvidijusParsiunas <oparsiunas@googlemail.com>
2022-03-04 20:05:39 +0000

    responsible elements css code refactoring

--- website/src/components/columns/middleColumn/iconModePanel/iconModePanelUtil.ts
+++ website/src/components/columns/middleColumn/iconModePanel/iconModePanelUtil.ts
@@ -1,7 +1,7 @@
 import { InputTypes } from '../../../../shared/consts/inputTypes';
 
 export class IconModePanelUtil {
-  // please note that display is also controlled by the @media (max-height: 380px), (max-width: 920px) query
+  // please note that display is also controlled by the @media (max-height: 380px), (max-width: 1000px) query
   // in the IconModePanel.css file which hides the panel when the screen is too small
   public static isIsDisplayed(activeInputType: InputTypes): boolean {
     return activeInputType === InputTypes.BASIC_COLOR;
